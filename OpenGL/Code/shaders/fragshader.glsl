ec8225c50686304a8e29dfd611c60b7ee51039b4
Yannick Stoffers <y.stoffers@rug.nl>
2017-02-21 14:21:41 +0100

    Pre shader changes

--- OpenGL/Code/shaders/fragshader.glsl
+++ OpenGL/Code/shaders/fragshader.glsl
@@ -13,14 +13,10 @@
 
 // Specify the output of the fragment shader
 // Usually a vec4 describing a color (Red, Green, Blue, Alpha/Transparency)
-// Fragment shader
-
-layout (location = 1) in vec3 vertcolour_in;
-
 out vec4 fColor;
 
 void main()
 {
-    fColor = vec4(vertcolour_in, 1.0);
+    fColor = vec4(1.0, 1.0, 1.0, 1.0);
 
 }
