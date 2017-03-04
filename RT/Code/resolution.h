67f7f2279e40c5d541824183d0103948aefc2bd6
Yannick Stoffers <y.stoffers@rug.nl>
2017-03-04 18:29:49 +0100

    Camera parsing from YAML.

---
+++ RT/Code/resolution.h
@@ -0,0 +1,16 @@
+
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+class Resolution
+{
+public:
+	Resolution (int width = 400, int height = 400);
+
+	struct {
+		int width;
+		int height;
+	};
+};
+
+#endif
